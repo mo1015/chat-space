2b94d39adca6207ff83cf3186d93644dbc844950
mo1015 <mieko.okumura@di-v.co.jp>
2020-07-10 17:50:06 +0900

    group_new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  root 'groups#index'
   root "messages#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
+
 end
